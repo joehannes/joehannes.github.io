5297d7482389911a00d99c26077b8a846f433a5c
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2020-09-19 21:37:21 +0200

    avatar svg and simple avatar fc

---
+++ src/components/Avatar/avatar.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+const Avatar: FC<{}> = () => {
+  return (
+    <img alt="avatar of joehannes" src="/assets/avatar.gif" width="128px" height="128px" />
+  );
+}
+
+export default Avatar;
