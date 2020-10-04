31e2df1faa796273f5e53c570fe28f93f2be194f
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2020-10-04 09:50:51 +0200

    bare angular skeleton

--- src/components/Avatar/avatar.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-const Avatar: FC<{}> = () => {
-  return (
-    <img alt="avatar of joehannes" src="/assets/avatar.gif" width="128px" height="128px" />
-  );
-}
-
-export default Avatar;
