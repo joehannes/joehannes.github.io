ee2f4525b1c01b16a00eca94279619d1f9b14cf2
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2021-01-21 21:41:08 +0100

    fire in tha branding

--- src/components/Branding.tsx
+++ src/components/Branding.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useRef } from 'react';
 import { animated, useSpring, useChain } from 'react-spring';
 
+import styles from './Branding.module.scss';
 import jsonData from '../assets/data/branding.json';
 
 interface IBrandingProps {
@@ -12,12 +13,14 @@ interface IBrandingProps {
 const Branding: FC<IBrandingProps> = ({ style }) => {
   const strokeAniRef = useRef();
   const fillAniRef = useRef();
+  const fireAniRef = useRef();
   const strokeAni = useSpring({ x: 0, from: { x: -1620}, ref: strokeAniRef, config: { duration: 3000 } });
-  const fillAni = useSpring({ o: 1, from: { o: 0 }, ref: fillAniRef, config: { duration: 3000 } });
+  const fillAni = useSpring({ o: 0.3, from: { o: 0 }, ref: fillAniRef, config: { duration: 3000 } });
+  const fireAni = useSpring({ o: 1, from: { o: 0 }, ref: fireAniRef, config: { duration: 3000 } });
 
-  useChain([strokeAniRef, fillAniRef]);
+  useChain([strokeAniRef, fillAniRef, fireAniRef]);
 
-  return (
+  return (<>
     <animated.svg
       height="110px"
       fill="black"
@@ -28,10 +31,14 @@ const Branding: FC<IBrandingProps> = ({ style }) => {
       strokeLinejoin="round"
       strokeWidth="1"
       strokeDashoffset={strokeAni.x}
+      className={styles.branding}
     >
       <path  d={jsonData.path} />
     </animated.svg>
-  );
+    <animated.div className={styles.fire} style={{ opacity: fireAni.o }}>
+      {Array(50).fill(0).map((x, i) => (<div key={x} className={styles.particle} />))}
+    </animated.div>
+  </>);
 }
 
 export default Branding;
