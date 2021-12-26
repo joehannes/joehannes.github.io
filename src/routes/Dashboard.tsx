aa650b1fd1d6b6207a4ff95a1da4db225f2b836c
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2021-12-26 18:56:50 -0400

    formatting;

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,34 +1,25 @@
-import React, { FC } from 'react';
-import {
-  Container,
-} from 'semantic-ui-react';
+import React, { FC } from "react";
+import { Container } from "semantic-ui-react";
 
-import Welcome from '../pages/Welcome';
-import Motivation from '../pages/Motivation';
+import Welcome from "../pages/Welcome";
+import Motivation from "../pages/Motivation";
 // import TechStack from '../pages/TechStack';
-import WorkExperience from '../pages/WorkExperience';
+import WorkExperience from "../pages/WorkExperience";
 
 const Dashboard: FC = () => {
-  return (<>
-    <Container
-      fluid
-      className="page__container"
-    >
-      <Welcome />
-    </Container>
-    <Container
-      fluid
-      className="page__container"
-    >
-      <Motivation />
-    </Container>
-    <Container
-      fluid
-      className="page__container"
-    >
-      <WorkExperience />
-    </Container>
-  </>);
-}
+  return (
+    <>
+      <Container fluid className="page__container">
+        <Welcome />
+      </Container>
+      <Container fluid className="page__container">
+        <Motivation />
+      </Container>
+      <Container fluid className="page__container">
+        <WorkExperience />
+      </Container>
+    </>
+  );
+};
 
 export default Dashboard;
