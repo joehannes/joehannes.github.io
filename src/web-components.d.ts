9e398302bb0f0ea13e760a47911372b429e428cb
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2021-12-27 01:00:03 -0400

    module type decl heatmap comp

--- src/web-components.d.ts
+++ src/web-components.d.ts
@@ -1,7 +1,7 @@
-declare module 'graphql-react';
-declare module 'react-vertical-timeline-component';
-declare module '*.mp3';
-declare module 'react-custom-scrollbars';
-declare module 'react-spring';
-declare module 'pouchdb-adapter-idb';
-declare module 'react-github-contributions-component';
+declare module "graphql-react";
+declare module "react-vertical-timeline-component";
+declare module "*.mp3";
+declare module "react-custom-scrollbars";
+declare module "react-spring";
+declare module "pouchdb-adapter-idb";
+declare module "react-calendar-heatmap";
