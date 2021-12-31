4ee28b4d96934f194ca7ffabf34a8ac26766ba21
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2021-12-30 22:45:42 -0400

    work-exp model update: united-signals, in-house

--- src/assets/data/db/WorkExperience.model.ts
+++ src/assets/data/db/WorkExperience.model.ts
@@ -1,247 +1,307 @@
-const WorkExperience = [{
-  id: "1",
-  style: {
-    background: "#46bde8",
-    color: "#ffffff"
+const WorkExperience = [
+  {
+    id: "1",
+    style: {
+      background: "#46bde8",
+      color: "#ffffff",
+    },
+    date: {
+      from: 1543618800000,
+      to: 1567288800000,
+    },
+    technology: ["1", "2", "3", "4", "6", "8"],
+    position: "1",
+    remote: "7",
+    contract: "1",
+    company: "1",
+    description: {
+      function: "PaaS for insurance brokers",
+      teamsize: "startup; small Team of about 5 developers",
+      method: "agile; remote online collaboration and communication",
+      focus: "dynamic form components for individual insurance contracts",
+    },
   },
-  date: {
-    from: 1543618800000,
-    to: 1567288800000,
+  {
+    id: "2",
+    style: {
+      background: "#E06C9F",
+      color: "#000000",
+    },
+    date: {
+      from: 1551394800000,
+      to: 1559340000000,
+    },
+    technology: ["1", "2", "3", "4", "6", "9", "15", "16", "17"],
+    position: "3",
+    remote: "7",
+    contract: "1",
+    company: "2",
+    description: {
+      function: "prototype for customer aquisition",
+      size: "startup; small Team of about 3 developers",
+      method: "agile; remote online collaboration and communication",
+      focus: "jack of all trades @frontend",
+    },
   },
-  technology: ["1", "2", "3", "4", "6", "8"],
-  position: "1",
-  remote: "7",
-  contract: "1",
-  company: "1",
-  description: {
-    function: "PaaS for insurance brokers",
-    teamsize: "startup; small Team of about 5 developers",
-    method: "agile; remote online collaboration and communication",
-    focus: "dynamic form components for individual insurance contracts"
+  {
+    id: "3",
+    style: {
+      background: "#00003F",
+      color: "#FFFFFF",
+    },
+    date: {
+      from: 1501538400000,
+      to: 1514761200000,
+    },
+    technology: ["1", "2", "3", "4", "6", "10"],
+    position: "6",
+    remote: "8",
+    contract: "1",
+    company: "3",
+    description: {
+      function: "prototyping",
+      size: "startup; small Team of about 3 people",
+      method: "freestyle; remote online collaboration and communication",
+      focus: "jack of all trades @frontend; advisor to CEO/COO;",
+    },
   },
-}, {
-  id: "2",
-  style: {
-    background: "#E06C9F",
-    color: "#000000",
+  {
+    id: "4",
+    style: {
+      background: "#BCE4FA",
+      color: "#000000",
+    },
+    date: {
+      from: 1472680800000,
+      to: 1488322800000,
+    },
+    technology: ["11", "2", "4", "13", "14", "12"],
+    position: "1",
+    contract: "1",
+    company: "4",
+    description: {
+      function: "bug squashing, repair, development",
+      size: "huge; medium sized team of about 8 people",
+      method: "daily scrum; kanban boards; game card group estimations",
+      focus: "fix a broken app; find needle in haystack",
+    },
   },
-  date: {
-    from: 1551394800000,
-    to: 1559340000000,
+  {
+    id: "5",
+    style: {
+      background: "#f5ab19",
+      color: "#ffffff",
+    },
+    date: {
+      from: 1406844000000,
+      to: 1420066800000,
+    },
+    technology: ["11", "18", "4", "5", "20", "19"],
+    position: "1",
+    remote: "8",
+    contract: "1",
+    company: "5",
+    description: {
+      function: "prototyping",
+      size: "at the time small to medium; distributed small agile team",
+      method: "agile; close cooperation with chief backender",
+      focus:
+        "dashboard for the digital transformation of terrestrial radio-shows; WebRTC integration; audio stream manipulation;",
+    },
   },
-  technology: ["1", "2", "3", "4", "6", "9", "15", "16", "17"],
-  position: "3",
-  remote: "7",
-  contract: "1",
-  company: "2",
-  description: {
-    function: "prototype for customer aquisition",
-    size: "startup; small Team of about 3 developers",
-    method: "agile; remote online collaboration and communication",
-    focus: "jack of all trades @frontend",
+  {
+    id: "6",
+    style: {
+      background: "#d03030",
+      color: "#ffffff",
+    },
+    date: {
+      from: 1388530800000,
+      to: 1398895200000,
+    },
+    technology: ["11", "18", "4"],
+    position: "1",
+    contract: "1",
+    company: "6",
+    description: {
+      function: "frontend implementation greenfield student project",
+      size: "at the time very small startup;",
+      method: "agile; close cooperation with Backend Lead and CTO",
+      focus:
+        "implementing a student exchange platform for the biggest international student org",
+    },
   },
-}, {
-  id: "3",
-  style: {
-    background: "#00003F",
-    color: "#FFFFFF",
+  {
+    id: "7",
+    style: {
+      background: "#c70f80",
+      color: "#ffffff",
+    },
+    date: {
+      from: 13779864000000,
+      to: 1388530800000,
+    },
+    technology: ["30", "18", "11", "5"],
+    position: "1",
+    contract: "1",
+    company: "7",
+    description: {
+      function: "frontend implementations; dashboard analytics",
+      size: "small startup",
+      method: "agile; close cooperation with COO and backenders",
+      focus: "implementing an analytics dashboard and scraping engine frontend",
+    },
   },
-  date: {
-    from: 1501538400000,
-    to: 1514761200000,
+  {
+    id: "8",
+    style: {
+      background: "#c12121",
+      color: "#ffffff",
+    },
+    date: {
+      from: 1362092400000,
+      to: new Date(2013, 6).getTime(),
+    },
+    technology: ["11", "21", "14"],
+    position: "7",
+    contract: "2",
+    company: "8",
+    description: {
+      function: "financial widgets; prototyping",
+      size: "medium to large business",
+      method: "agile",
+      focus: "sprint efforts in developing widgets and prototyping in R&D",
+    },
   },
-  technology: ["1", "2", "3", "4", "6", "10"],
-  position: "6",
-  remote: "8",
-  contract: "1",
-  company: "3",
-  description: {
-    function: "prototyping",
-    size: "startup; small Team of about 3 people",
-    method: "freestyle; remote online collaboration and communication",
-    focus: "jack of all trades @frontend; advisor to CEO/COO;"
+  {
+    id: "9",
+    style: {
+      background: "#97C127",
+      color: "#000000",
+    },
+    date: {
+      from: 1341093600000,
+      to: 1359673200000,
+    },
+    technology: ["30", "18", "22", "5", "23"],
+    position: "7",
+    contract: "1",
+    company: "9",
+    description: {
+      function: "refactoring a flash app to javascript",
+      size: "medium sized business",
+      method: "freestyle",
+      focus: "charting solutions; porting flash software",
+    },
   },
-}, {
-  id: "4",
-  style: {
-    background: "#BCE4FA",
-    color: "#000000",
+  {
+    id: "10",
+    style: {
+      background: "#004178",
+      color: "#000000",
+    },
+    date: {
+      from: 1325372400000,
+      to: 1341093600000,
+    },
+    technology: ["30", "21"],
+    position: "8",
+    contract: "2",
+    company: "10",
+    description: {
+      function: "junior widget developer",
+      size: "large sized business; medium sized team",
+      method: "scrum",
+      focus:
+        "developing on demand custom jQuery widgets for seamless integration into main software",
+    },
   },
-  date: {
-    from: 1472680800000,
-    to: 1488322800000,
+  {
+    id: "11",
+    style: {
+      background: "#138C8F",
+      color: "#FFFFFF",
+    },
+    date: {
+      from: 1293836400000,
+      to: 1322694000000,
+    },
+    technology: ["30", "21", "26", "24", "25"],
+    position: "9",
+    contract: "2",
+    company: "11",
+    description: {
+      function: "daily tasks of a junior",
+      size: "medium sized business; medium sized team",
+      method: "freestyle with agile touch",
+      focus:
+        "mostly developing an AJAX-microsite for sustainable seafood certification",
+    },
   },
-  technology: ["11", "2", "4", "13", "14", "12"],
-  position: "1",
-  contract: "1",
-  company: "4",
-  description: {
-    function: "bug squashing, repair, development",
-    size: "huge; medium sized team of about 8 people",
-    method: "daily scrum; kanban boards; game card group estimations",
-    focus: "fix a broken app; find needle in haystack"
+  {
+    id: "12",
+    style: {
+      background: "#3030D0",
+      color: "#000000",
+    },
+    date: {
+      from: 1230764400000,
+      to: 1262300400000,
+    },
+    technology: ["27", "28", "29", "19"],
+    position: "10",
+    contract: "2",
+    company: "12",
+    description: {
+      function: "issue ticker; tech backoffice support",
+      size: "small sized business; no specific team forms",
+      method: "freestyle",
+      focus: "mostly hacking whatever micro-issue popped up",
+    },
   },
-}, {
-  id: "5",
-  style: {
-    background: "#f5ab19",
-    color: "#ffffff",
+  {
+    id: "13",
+    style: {
+      background: "#EE7B1B",
+      color: "#FFFFFF",
+    },
+    date: {
+      from: new Date(2021, 2).getTime(),
+      to: new Date(2021, 12).getTime(),
+    },
+    technology: ["1", "30", "2", "31", "32", "33", "7", "28"],
+    position: "1",
+    contract: "1",
+    company: "13",
+    description: {
+      function: "research; greenfield; tech-stack feasability; hands-on",
+      size: "small to medium sized business; core business team; new platform - new team",
+      method: "core team agile; greenfield freestyle",
+      focus: "research; solid foundation; dynamic forms;",
+    },
   },
-  date: {
-    from: 1406844000000,
-    to: 1420066800000,
+  {
+    id: "14",
+    style: {
+      background: "#000000",
+      color: "#FFFFFF",
+    },
+    date: {
+      from: new Date(2021, 5).getTime(),
+      to: new Date(2021, 12).getTime(),
+    },
+    technology: ["1", "30", "3", "8", "7", "34", "35"],
+    position: "1",
+    contract: "1",
+    company: "14",
+    description: {
+      function: "management; product-owner; architect;",
+      size: "small to medium sized business; early-equity; start-up; unicorn;",
+      method: "agile",
+      focus:
+        "MVP; go-live; deadline; management; mentoring; hand-on; code reviews;",
+    },
   },
-  technology: ["11", "18", "4", "5", "20", "19"],
-  position: "1",
-  remote: "8",
-  contract: "1",
-  company: "5",
-  description: {
-    function: "prototyping",
-    size: "at the time small to medium; distributed small agile team",
-    method: "agile; close cooperation with chief backender",
-    focus: "dashboard for the digital transformation of terrestrial radio-shows; WebRTC integration; audio stream manipulation;"
-  },
-}, {
-  id: "6",
-  style: {
-    background: "#d03030",
-    color: "#ffffff",
-  },
-  date: {
-    from: 1388530800000,
-    to: 1398895200000,
-  },
-  technology: ["11", "18", "4"],
-  position: "1",
-  contract: "1",
-  company: "6",
-  description: {
-    function: "frontend implementation greenfield student project",
-    size: "at the time very small startup;",
-    method: "agile; close cooperation with Backend Lead and CTO",
-    focus: "implementing a student exchange platform for the biggest international student org"
-  },
-}, {
-  id: "7",
-  style: {
-    background: "#c70f80",
-    color: "#ffffff"
-  },
-  date: {
-    from: 13779864000000,
-    to: 1388530800000,
-  },
-  technology: ["30", "18", "11", "5"],
-  position: "1",
-  contract: "1",
-  company: "7",
-  description: {
-    function: "frontend implementations; dashboard analytics",
-    size: "small startup",
-    method: "agile; close cooperation with COO and backenders",
-    focus: "implementing an analytics dashboard and scraping engine frontend"
-  },
-}, {
-  id: "8",
-  style: {
-    background: "#c12121",
-    color: "#ffffff",
-  },
-  date: {
-    from: 1362092400000,
-    to: 13726296000000,
-  },
-  technology: ["11", "21", "14"],
-  position: "7",
-  contract: "2",
-  company: "8",
-  description: {
-    function: "financial widgets; prototyping",
-    size: "medium to large business",
-    method: "agile",
-    focus: "sprint efforts in developing widgets and prototyping in R&D"
-  },
-}, {
-  id: "9",
-  style: {
-    background: "#97C127",
-    color: "#000000",
-  },
-  date: {
-    from: 1341093600000,
-    to: 1359673200000,
-  },
-  technology: ["30", "18", "22", "5", "23"],
-  position: "7",
-  contract: "1",
-  company: "9",
-  description: {
-    function: "refactoring a flash app to javascript",
-    size: "medium sized business",
-    method: "freestyle",
-    focus: "charting solutions; porting flash software"
-  },
-}, {
-  id: "10",
-  style: {
-    background: "#004178",
-    color: "#000000",
-  },
-  date: {
-    from: 1325372400000,
-    to: 1341093600000,
-  },
-  technology: ["30", "21"],
-  position: "8",
-  contract: "2",
-  company: "10",
-  description: {
-    function: "junior widget developer",
-    size: "large sized business; medium sized team",
-    method: "scrum",
-    focus: "developing on demand custom jQuery widgets for seamless integration into main software"
-  },
-}, {
-  id: "11",
-  style: {
-    background: "#138C8F",
-    color: "#FFFFFF",
-  },
-  date: {
-    from: 1293836400000,
-    to: 1322694000000,
-  },
-  technology: ["30", "21", "26", "24", "25"],
-  position: "9",
-  contract: "2",
-  company: "11",
-  description: {
-    function: "daily tasks of a junior",
-    size: "medium sized business; medium sized team",
-    method: "freestyle with agile touch",
-    focus: "mostly developing an AJAX-microsite for sustainable seafood certification"
-  },
-}, {
-  id: "12",
-  style: {
-    background: "#3030D0",
-    color: "#000000",
-  },
-  date: {
-    from: 1230764400000,
-    to: 1262300400000,
-  },
-  technology: ["27", "28", "29", "19"],
-  position: "10",
-  contract: "2",
-  company: "12",
-  description: {
-    function: "issue ticker; tech backoffice support",
-    size: "small sized business; no specific team forms",
-    method: "freestyle",
-    focus: "mostly hacking whatever micro-issue popped up"
-  },
-}];
+];
 
 export default WorkExperience;
